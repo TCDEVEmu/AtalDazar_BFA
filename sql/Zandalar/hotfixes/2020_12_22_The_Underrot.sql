f5a5e91086d17a7011a3cc854900fddbaa17ba2f
Mistix <mistix20@gmail.com>
2020-12-22 20:00:35 -0800

    Missing things from The Underrot

---
+++ sql/Zandalar/hotfixes/2020_12_22_The_Underrot.sql
@@ -0,0 +1,11 @@
+DELETE FROM `broadcast_text` WHERE `ID` IN (157862, 151961, 151962, 151954, 151953, 151951, 151952, 151950, 151949);
+INSERT INTO `broadcast_text` (`Text`, `Text1`, `ID`, `LanguageID`, `ConditionID`, `EmotesID`, `Flags`, `ChatBubbleDurationMs`, `SoundEntriesID1`, `SoundEntriesID2`, `EmoteID1`, `EmoteID2`, `EmoteID3`, `EmoteDelay1`, `EmoteDelay2`, `EmoteDelay3`, `VerifiedBuild`) VALUES
+('Much was accomplished here today. We can regroup outside.', 'Much was accomplished here today. We can regroup outside.', 157862, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 31478),
+('', 'De titan bought us time, but G\'huun will not stay contained for long.', 151961, 0, 0, 0, 0, 0, 0, 106855, 0, 0, 0, 0, 0, 0, 31478),
+('', 'We must gather our forces and stop de rot at its source.', 151962, 0, 0, 0, 0, 0, 0, 106856, 0, 0, 0, 0, 0, 0, 31478),
+('', 'If dis spreads to de surface, we will not be able to contain it.', 151954, 0, 0, 0, 0, 0, 0, 106853, 0, 0, 0, 0, 0, 0, 31478),
+('', 'I have never seen dis form of corruption before.', 151953, 0, 0, 0, 0, 0, 0, 106877, 0, 0, 0, 0, 0, 0, 31478),
+('', 'Ack! What manner of creature is dis?!', 151951, 0, 0, 0, 0, 0, 0, 106852, 0, 0, 0, 0, 0, 0, 31478),
+('', 'It must not be allowed to reach de surface! Destroy it now!', 151952, 0, 0, 0, 0, 0, 0, 106876, 0, 0, 0, 0, 0, 0, 31478),
+('', 'Guard yourselves. Who knows what lurks in dese caves.', 151950, 0, 0, 0, 0, 0, 0, 106858, 0, 0, 0, 0, 0, 0, 31478),
+('', 'De blood trolls are scattered, but we must find de source of de rot and destroy it.', 151949, 0, 0, 0, 0, 0, 0, 106857, 0, 0, 0, 0, 0, 0, 31478);
