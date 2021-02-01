09f5ab94aada01ff21e3948338b715088c7abad9
Hdeste70 <65082916+hdeste70@users.noreply.github.com>
2021-02-01 17:18:40 +0100

    FIX ORC MARGAR START ZONE

---
+++ sql/Zandalar/world/2021_08_01_Fix_Correct_Orc_Margar_Start_Zone.sql
@@ -0,0 +1,10 @@
+/*Hdeste70 Correct Mag'har Orc start position */
+DELETE FROM playercreateinfo WHERE race = '36';
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES ('36', '6', '2297', '12952', '481.941', '-2124.49', '840.857', '3.09056');
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES ('36', '10', '1', '1637', '1966.27', '-4743.52', '37.178', '1.281');
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES ('36', '8', '1', '1637', '1966.27', '-4743.52', '37.178', '1.281');
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES ('36', '7', '1', '1637', '1966.27', '-4743.52', '37.178', '1.281');
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES ('36', '5', '1', '1637', '1966.27', '-4743.52', '37.178', '1.281');
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES ('36', '4', '1', '1637', '1966.27', '-4743.52', '37.178', '1.281');
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES ('36', '3', '1', '1637', '1966.27', '-4743.52', '37.178', '1.281');
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES ('36', '1', '1', '1637', '1966.27', '-4743.52', '37.178', '1.281');
