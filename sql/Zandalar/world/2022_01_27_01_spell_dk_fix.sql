0a3b5282bf08a88628afdd0a3eb1c30df43e7ebe
MistiX <mistix20@gmail.com>
2022-01-27 20:52:41 -0500

    Spell/ Scripting Spell DK Zarpa https://es.wowhead.com/spell=47468/zarpa

---
+++ sql/Zandalar/world/2022_01_27_01_spell_dk_fix.sql
@@ -0,0 +1,5 @@
+/*Here all spell clace DK fixes will be added*/
+
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_dk_ghoul_claw';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(47468, 'spell_dk_ghoul_claw'),
