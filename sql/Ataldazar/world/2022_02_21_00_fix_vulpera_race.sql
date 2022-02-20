8771c1d5e5a373f5bde95ae91a3f1e48136ad1d0
SargeroDeV <neohd251535@gmail.com>
2022-02-20 22:15:21 +0100

    DB: Fixed vulpera starting positions and vulpera hunter starterbar. Credits Steyfix

---
+++ sql/Ataldazar/world/2022_02_21_00_fix_vulpera_race.sql
@@ -0,0 +1,39 @@
+-- Action bars Hunter
+DELETE FROM `playercreateinfo_action` WHERE `race`=35 AND `class`=3 AND `button`=1;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (35, 3, 1, 34026, 0);
+DELETE FROM `playercreateinfo_action` WHERE `race`=35 AND `class`=3 AND `button`=0;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (35, 3, 0, 193455, 0);
+DELETE FROM `playercreateinfo_action` WHERE `race`=35 AND `class`=3 AND `button`=11;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (35, 3, 11, 312411, 0);
+DELETE FROM `playercreateinfo_action` WHERE `race`=35 AND `class`=3 AND `button`=2;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (35, 3, 2, 217200, 0);
+DELETE FROM `playercreateinfo_action` WHERE `race`=35 AND `class`=3 AND `button`=3;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (35, 3, 3, 2643, 0);
+DELETE FROM `playercreateinfo_action` WHERE `race`=35 AND `class`=3 AND `button`=9;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (35, 3, 9, 312372, 0);
+DELETE FROM `playercreateinfo_action` WHERE `race`=35 AND `class`=3 AND `button`=10;
+INSERT INTO `playercreateinfo_action` (`race`, `class`, `button`, `action`, `type`) VALUES (35, 3, 10, 312370, 48);
+
+-- Starter positions
+DELETE FROM `playercreateinfo` WHERE `race`=35 AND `class`=9;
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (35, 9, 1, 1637, 1947.03, -4632.14, 34.0998, 3.67048);
+DELETE FROM `playercreateinfo` WHERE `race`=35 AND `class`=8;
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (35, 8, 1, 1637, 1947.03, -4632.14, 34.0998, 3.67048);
+DELETE FROM `playercreateinfo` WHERE `race`=35 AND `class`=3;
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (35, 3, 1, 1637, 1947.03, -4632.14, 34.0998, 3.67048);
+DELETE FROM `playercreateinfo` WHERE `race`=35 AND `class`=4;
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (35, 4, 1, 1637, 1947.03, -4632.14, 34.0998, 3.67048);
+DELETE FROM `playercreateinfo` WHERE `race`=35 AND `class`=6;
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (35, 6, 1, 1637, 1947.03, -4632.14, 34.0998, 3.67048);
+DELETE FROM `playercreateinfo` WHERE `race`=35 AND `class`=7;
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (35, 7, 1, 1637, 1947.03, -4632.14, 34.0998, 3.67048);
+DELETE FROM `playercreateinfo` WHERE `race`=35 AND `class`=2;
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (35, 2, 1, 1637, 1947.03, -4632.14, 34.0998, 3.67048);
+DELETE FROM `playercreateinfo` WHERE `race`=35 AND `class`=10;
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (35, 10, 1, 1637, 1947.03, -4632.14, 34.0998, 3.67048);
+DELETE FROM `playercreateinfo` WHERE `race`=35 AND `class`=11;
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (35, 11, 1, 1637, 1947.03, -4632.14, 34.0998, 3.67048);
+DELETE FROM `playercreateinfo` WHERE `race`=35 AND `class`=5;
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (35, 5, 1, 1637, 1947.03, -4632.14, 34.0998, 3.67048);
+DELETE FROM `playercreateinfo` WHERE `race`=35 AND `class`=1;
+INSERT INTO `playercreateinfo` (`race`, `class`, `map`, `zone`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (35, 1, 1, 1637, 1947.03, -4632.14, 34.0998, 3.67048);
