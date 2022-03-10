7e49e4f2e4ad25c81fdfe4c9a8f71dbeacdced3f
SargeroDeV <neohd251535@gmail.com>
2022-03-10 22:16:29 +0100

    Spells: Fix Azerite spells 280713, 269239, 269238

---
+++ sql/Ataldazar/world/2022_03_10_01_spells_azerite.sql
@@ -0,0 +1,6 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (280713, 269239, 269238);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(280713, 'spell_champion_of_azeroth'),
+(269239, 'spell_vampiric_speed_speed'),
+(269238, 'spell_vampiric_speed_heal');
+
--- src/server/scripts/Spells/spell_azerite.cpp
+++ src/server/scripts/Spells/spell_azerite.cpp
@@ -38,7 +38,126 @@ enum SpellIds
 
 };
 
+// 280713 Champion of Azeroth
+class spell_champion_of_azeroth : public SpellScriptLoader
+{
+public:
+    spell_champion_of_azeroth() : SpellScriptLoader("spell_champion_of_azeroth") { }
+
+    class spell_champion_of_azeroth_AuraScript : public AuraScript
+    {
+        PrepareAuraScript(spell_champion_of_azeroth_AuraScript);
+
+        bool Validate(SpellInfo const* /*spell*/) override
+        {
+            return ValidateSpellInfo({ 280710 });
+        }
+
+        void CalculateAmount(AuraEffect const* /*aurEff*/, int32& amount, bool& /*canBeRecalculated*/)
+        {
+            if (Unit * caster = GetCaster())
+            {
+                if (AuraEffect * aurEf = caster->GetAuraEffect(280710, EFFECT_0)) // Champion of Azeroth 
+                {
+                    amount = aurEf->GetAmount();
+                }
+            }
+        }
+
+        void Register() override
+        {
+            DoEffectCalcAmount += AuraEffectCalcAmountFn(spell_champion_of_azeroth_AuraScript::CalculateAmount, EFFECT_0, SPELL_AURA_MOD_RATING);
+            DoEffectCalcAmount += AuraEffectCalcAmountFn(spell_champion_of_azeroth_AuraScript::CalculateAmount, EFFECT_1, SPELL_AURA_MOD_RATING);
+            DoEffectCalcAmount += AuraEffectCalcAmountFn(spell_champion_of_azeroth_AuraScript::CalculateAmount, EFFECT_2, SPELL_AURA_MOD_RATING);
+            DoEffectCalcAmount += AuraEffectCalcAmountFn(spell_champion_of_azeroth_AuraScript::CalculateAmount, EFFECT_3, SPELL_AURA_MOD_RATING);
+        }
+    };
+
+    AuraScript* GetAuraScript() const override
+    {
+        return new spell_champion_of_azeroth_AuraScript();
+    }
+};
+
+// 269239 Vampiric Speed
+class spell_vampiric_speed_speed : public SpellScriptLoader
+{
+public:
+    spell_vampiric_speed_speed() : SpellScriptLoader("spell_vampiric_speed_speed") { }
+
+    class spell_vampiric_speed_speed_AuraScript : public AuraScript
+    {
+        PrepareAuraScript(spell_vampiric_speed_speed_AuraScript);
+
+        bool Validate(SpellInfo const* /*spell*/) override
+        {
+            return ValidateSpellInfo({ 268599 });
+        }
+
+        void CalculateAmount(AuraEffect const* /*aurEff*/, int32& amount, bool& /*canBeRecalculated*/)
+        {
+            if (Unit * caster = GetCaster())
+            {
+                if (AuraEffect * aurEf = caster->GetAuraEffect(268599, EFFECT_1)) // Vampiric Speed
+                {
+                    amount = aurEf->GetAmount();
+                }
+            }
+        }
+
+        void Register() override
+        {
+            DoEffectCalcAmount += AuraEffectCalcAmountFn(spell_vampiric_speed_speed_AuraScript::CalculateAmount, EFFECT_0, SPELL_AURA_MOD_RATING);
+        }
+    };
+
+    AuraScript* GetAuraScript() const override
+    {
+        return new spell_vampiric_speed_speed_AuraScript();
+    }
+};
+
+// 269238 Vampiric Speed
+class spell_vampiric_speed_heal : public SpellScriptLoader
+{
+public:
+    spell_vampiric_speed_heal() : SpellScriptLoader("spell_vampiric_speed_heal") { }
+
+    class spell_vampiric_speed_heal_SpellScript : public SpellScript
+    {
+        PrepareSpellScript(spell_vampiric_speed_heal_SpellScript);
+
+        bool Validate(SpellInfo const* /*spell*/) override
+        {
+            return ValidateSpellInfo({ 268599 });
+        }
+
+        void HandleHeal(SpellEffIndex /*effIndex*/)
+        {
+            if (Unit * caster = GetCaster())
+            {
+                if (AuraEffect * aurEf = caster->GetAuraEffect(268599, EFFECT_0)) // Vampiric Speed
+                {
+                    SetHitHeal(aurEf->GetAmount());
+                }
+            }
+        }
+        void Register() override
+        {
+            OnEffectHitTarget += SpellEffectFn(spell_vampiric_speed_heal_SpellScript::HandleHeal, EFFECT_0, SPELL_EFFECT_HEAL);
+        }
+
+    };
+
+    SpellScript* GetSpellScript() const override
+    {
+        return new spell_vampiric_speed_heal_SpellScript();
+    }
+};
+
 void AddSC_azerite_spell_scripts()
 {
-
+    new spell_champion_of_azeroth();
+    new spell_vampiric_speed_speed();
+    new spell_vampiric_speed_heal();
 }
