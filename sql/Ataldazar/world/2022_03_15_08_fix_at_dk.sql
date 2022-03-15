38ae367e5e9ac1dc554459d4c9dad66021a04a57
SargeroDeV <neohd251535@gmail.com>
2022-03-15 15:24:53 +0100

    Spell: Fix at_dk_death_and_decay

---
+++ sql/Ataldazar/world/2022_03_15_08_fix_at_dk.sql
@@ -0,0 +1,5 @@
+REPLACE INTO `areatrigger_template` (`Id`, `Type`, `Flags`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `ScriptName`, `VerifiedBuild`) VALUES 
+('9225', '0', '0', '8', '8', '0', '0', '0', '0', 'at_dk_death_and_decay', '31478');
+REPLACE INTO `spell_areatrigger` (`SpellMiscId`, `AreaTriggerId`, `MoveCurveId`, `ScaleCurveId`, `MorphCurveId`, `FacingCurveId`, `AnimId`, `AnimKitId`, `DecalPropertiesId`, `TimeToTarget`, `TimeToTargetScale`, `VerifiedBuild`) VALUES 
+('4485', '9225', '0', '0', '0', '0', '0', '0', '0', '0', '10000', '32750');
+
