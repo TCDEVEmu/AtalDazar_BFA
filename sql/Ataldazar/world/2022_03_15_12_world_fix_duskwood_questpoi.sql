609322459056a60c6c043f27140f77c3f36dda63
SargeroDeV <neohd251535@gmail.com>
2022-03-15 16:12:05 +0100

    DB/Questpoi : Fix duskwood quest_poi and quest_poi_points

---
+++ sql/Ataldazar/world/2022_03_15_12_world_fix_duskwood_questpoi.sql
@@ -0,0 +1,69 @@
+DELETE FROM `quest_poi` WHERE `QuestID` = '26655'; 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26655, 0, 0, -1, 0, 0, 0, 47, 0, 1, 0, 0, 0, 0, 35662); 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26655, 0, 1, 0, 266284, 1596, 0, 47, 0, 1, 0, 0, 0, 0, 35662); 
+DELETE FROM `quest_poi_points` WHERE `QuestID` = '26655'; 
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26655, 1, 0, -10350, -1527, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26655, 0, 0, -10532, -1213, 35662);
+
+DELETE FROM `quest_poi` WHERE `QuestID` = '26685'; 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26685, 0, 0, -1, 0, 0, 0, 42, 0, 1, 0, 0, 0, 0, 35662); 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26685, 0, 1, 0, 266026, 1968, 0, 93, 0, 1, 0, 0, 0, 0, 35662); 
+DELETE FROM `quest_poi_points` WHERE `QuestID` = '26685'; 
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26685, 1, 0, -11233, -183, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26685, 0, 0, -10570, -1313, 35662);
+
+DELETE FROM `quest_poi` WHERE `QuestID` = '26707'; 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26707, 0, 0, 0, 266919, 60204, 0, 34, 0, 1, 0, 0, 0, 0, 24330);
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26707, 1, 1, -1, 0, 0, 0, 34, 0, 1, 0, 0, 0, 0, 24330);
+DELETE FROM `quest_poi_points` WHERE `QuestID` = '26707'; 
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26707, 0, 0, -11022, -660, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26707, 0, 1, -10988, -486, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26707, 0, 2, -10983, -454, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26707, 0, 3, -11045, -418, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26707, 0, 4, -11113, -457, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26707, 0, 5, -11073, -579, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26707, 1, 0, -10930, -380, 35662);
+
+DELETE FROM `quest_poi` WHERE `QuestID` = '26674'; 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26674, 0, 0, -1, 0, 0, 0, 47, 0, 1, 0, 0, 0, 0, 35662); 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26674, 0, 1, 0, 253786, 315, 0, 47, 0, 1, 0, 0, 0, 0, 35662); 
+DELETE FROM `quest_poi_points` WHERE `QuestID` = '26674'; 
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26674, 0, 0, -10512, -1301, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26674, 1, 0, -10372, -1252, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26674, 2, 0, -10532, -1213, 35662);
+
+DELETE FROM `quest_poi` WHERE `QuestID` = '26719'; 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26719, 0, 0, -1, 0, 0, 0, 47, 0, 1, 0, 0, 0, 0, 35662); 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26719, 0, 1, 0, 266656, 60205, 0, 47, 0, 1, 0, 0, 0, 0, 35662); 
+DELETE FROM `quest_poi_points` WHERE `QuestID` = '26719'; 
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26719, 1, 0, -10755, 339, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26719, 0, 0, -10930, -380, 35662);
+
+DELETE FROM `quest_poi` WHERE `QuestID` = '26676'; 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26676, 0, 0, -1, 0, 0, 0, 47, 0, 1, 0, 0, 0, 0, 35662); 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26676, 0, 1, 0, 260308, 1451, 0, 47, 0, 1, 0, 0, 0, 0, 35662); 
+DELETE FROM `quest_poi_points` WHERE `QuestID` = '26676'; 
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26676, 0, 0, -10350, -1527, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26676, 1, 0, -10517, -1159, 35662);
+
+DELETE FROM `quest_poi` WHERE `QuestID` = '26681'; 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26681, 0, 0, -1, 0, 0, 0, 42, 0, 1, 0, 0, 0, 0, 35662); 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26681, 0, 1, 0, 254006, 1637, 0, 42, 0, 1, 0, 0, 0, 0, 35662); 
+DELETE FROM `quest_poi_points` WHERE `QuestID` = '26681'; 
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26681, 1, 0, -10552, -1109, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26681, 0, 0, -10350, -1527, 35662);
+
+DELETE FROM `quest_poi` WHERE `QuestID` = '26795'; 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26795, 0, 0, -1, 0, 0, 0, 47, 0, 1, 0, 0, 0, 0, 35662); 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26795, 0, 1, 0, 266223, 3514, 0, 47, 0, 1, 0, 0, 0, 0, 35662); 
+DELETE FROM `quest_poi_points` WHERE `QuestID` = '26795'; 
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26795, 0, 0, -10560, -1152, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26795, 1, 0, -10395, 340, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26795, 2, 0, -10561, -1152, 35662);
+
+DELETE FROM `quest_poi` WHERE `QuestID` = '26797'; 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26797, 0, 0, -1, 0, 0, 0, 47, 0, 1, 0, 0, 0, 0, 35662); 
+INSERT INTO `quest_poi`(`QuestID`, `BlobIndex`, `Idx1`, `ObjectiveIndex`, `QuestObjectiveID`, `QuestObjectID`, `MapID`, `UiMapID`, `Priority`, `Flags`, `WorldEffectID`, `PlayerConditionID`, `SpawnTrackingID`, `AlwaysAllowMergingBlobs`, `VerifiedBuild`) VALUES (26797, 0, 1, 0, 265558, 2162, 0, 47, 0, 1, 0, 0, 0, 0, 35662); 
+DELETE FROM `quest_poi_points` WHERE `QuestID` = '26797'; 
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26797, 1, 0, -10240, 355, 35662);
+INSERT INTO `quest_poi_points`(`QuestID`, `Idx1`, `Idx2`, `X`, `Y`, `VerifiedBuild`) VALUES (26797, 0, 0, -10566, -1208, 35662);
